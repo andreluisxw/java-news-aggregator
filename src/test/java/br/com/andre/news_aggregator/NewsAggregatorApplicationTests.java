52bc5f1f4b3d967fe889f86f83646c0b0cea91b6
Andre Luis Maia <andreluis772233@gmail.com>
2025-09-05 13:01:24 -0300

    Initial commit: Configuração inicial do projeto backend

---
+++ src/test/java/br/com/andre/news_aggregator/NewsAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andre.news_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
